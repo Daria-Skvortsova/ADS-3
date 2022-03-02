9c507cd4f39992846ffd30119c0d6a0589f2bbb1
Daria-Skvortsova <99176875+Daria-Skvortsova@users.noreply.github.com>
2022-03-02 21:57:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,36 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int otvet = 0;
-  for (int i = 0; i < size; i++) {
-    if (arr[i] == value) {
-      otvet+=1;
-    }
+  int otvet, ceredina;
+  int granica_1 = 0;
+  int granica_2 = size-1;
+  while (granica_1 <= granica_2){
+    ceredina = granica_1 + ((granica_2 - granica_1)/2);
+    otvet = 0;
+    if (arr[ceredina] == value) {
+      for (int i = 0; arr[ceredina + i] == value; i++) {
+        otvet++;
+        if ((ceredina + i) == granica_2) {
+          break;
+        }
+      }
+      for (int i = 1; arr[ceredina - i] == value; i++) {
+        otvet++;
+        if (ceredina - i == 0) {
+          break;
+        }
+      }
+	  break;
+	} else if (arr[ceredina] > value) {
+      granica_2 = ceredina - 1;	
+      } else {
+        granica_1 = ceredina + 1;
+	    }
   }
   if (otvet == 0) {
     return 0;
-  } else {
-    return otvet;
-    }
+  }
+  else {
+      return otvet;
+  }
 }
